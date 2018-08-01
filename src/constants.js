cccce668987f1ef4fb31249691a6eb6f7518ccd4
Maciul <pawel.maciulewski@gmail.com>
2018-07-31 19:24:44 -0600

    add urls

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const PORT = 8002;
+const BITTREX_URL = 'https://bittrex.com/api/v1.1/public/getorderbook?market='; // BTC-ETH&type=both
+const POLONIEX_URL = 'https://poloniex.com/public?command=returnOrderBook&currencyPair='; // BTC_ETH&depth=10
 
 module.exports = {
-    PORT
+    PORT,
+    BITTREX_URL,
+    POLONIEX_URL
 };
